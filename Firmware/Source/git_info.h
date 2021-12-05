977e30e32fe3491bc2a191a4d5650be80839dac4
mlhzv2204 <rsln.mlhzv@gmail.com>
2021-12-05 16:36:07 +0300

    Добавлены файлы для считывания прошивки  блока

---
+++ Firmware/Source/git_info.h
@@ -0,0 +1,10 @@
+#ifndef __GIT_INFO_H
+#define __GIT_INFO_H
+
+#include "stdinc.h"
+
+static const char git_branch[] = "develop-PCBv1.1-MME404"; 
+static const char git_commit[] = "8b9b3ac563c40bbb3c7bfbcc1827f8191aaa11f1"; 
+static const char git_date[] = "2021/11/16T12:41:25"; 
+
+#endif // __GIT_INFO_H
